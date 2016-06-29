6500161188d5d8c1b5185711b6f520b1cf113780
ShengYue <ysc8620@163.com>
2016-06-29 10:12:36 +0800

    ds

--- Application/Home/Controller/LivedataController.class.php
+++ Application/Home/Controller/LivedataController.class.php
@@ -12,6 +12,8 @@ class LivedataController extends BaseController {
     public function index(){
         $data['post_data'] = file_get_contents("php://input");
         $data['post'] = $_POST;
+        $data['get'] = $_GET;
+        
         $str = date("Y-m-d H:i:s")."==========================================\r\n";
         $str .= json_encode($data);
         $str .= date("Y-m-d H:i:s")."==========================================\r\n";
