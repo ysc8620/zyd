aeedabf4a35e16927a9fb8163ccd1b797eb453ee
ShengYue <ysc8620@163.com>
2016-07-06 19:16:50 +0800

    ds

--- Application/Home/Controller/LivedataController.class.php
+++ Application/Home/Controller/LivedataController.class.php
@@ -10,14 +10,27 @@ class LivedataController extends BaseController {
      *
      */
     public function index(){
-        $data['post_data'] = file_get_contents("php://input");
-        $data['post'] = $_POST;
-        $data['get'] = $_GET;
-
-        $str = date("Y-m-d H:i:s")."==========================================\r\n";
-        $str .= json_encode($data)."\r\n";
-        $str .= date("Y-m-d H:i:s")."==========================================\r\n";
-        \Org\Util\File::write_file(APP_PATH .'/post.log',$str,'a+');
+        //$data['post_data'] = file_get_contents("php://input");
+        $data = $_POST['data'];
+        if($data){
+            if(strpos($data,'Soccer') === false){
+                $str = date("Y-m-d H:i:s")."==========================================\r\n";
+                $str .= json_encode($data)."\r\n";
+                $str .= date("Y-m-d H:i:s")."==========================================\r\n";
+                \Org\Util\File::write_file(APP_PATH .'/post.log',$str,'a+');
+            }else{
+                $str = date("Y-m-d H:i:s")."==========================================\r\n";
+                $str .= json_encode($data)."\r\n";
+                $str .= date("Y-m-d H:i:s")."==========================================\r\n";
+                \Org\Util\File::write_file(APP_PATH .'/other_post.log',$str,'a+');
+            }
+        }
+//        //$data['get'] = $_GET;
+//
+//        $str = date("Y-m-d H:i:s")."==========================================\r\n";
+//        $str .= json_encode($data)."\r\n";
+//        $str .= date("Y-m-d H:i:s")."==========================================\r\n";
+//        \Org\Util\File::write_file(APP_PATH .'/post.log',$str,'a+');
         echo "ok";
     }
 }
---
+++ t.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShengYue
+ * Date: 2016/7/6
+ * Time: 18:57
+ */
