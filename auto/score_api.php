1d1881e103c27a47d597dd215dfa4a2dba289a14
ShengYue <ysc8620@163.com>
2016-07-16 15:31:49 +0800

    ew

--- auto/score_api.php
+++ auto/score_api.php
@@ -22,39 +22,42 @@ do{
     $data = json_decode(json_encode($obj), true);
 
     foreach($data['match'] as $item){
-        $home = explode(',',trim($item['h']));
-        $away = explode(',',trim($item['i']));
-
+        $home = explode(',',trim($item['home']));
+        $away = explode(',',trim($item['away']));
+        $league = explode(',',trim($item['league']));
         $info = [
             'match_id' => $item['ID'],
             'color'     => $item['color'],
+            'league_id'  => $item['leagueID'],
+            'league_name' => $league[0],
             'league'    => $item['league'],
-            'time'      => $item['d'],
-            'kind'      => $item['e'],
-            'state'     => $item['f'],
-            'home'      => $item['h'],
+            'sub_league'    => $item['subLeague'],
+            'level'        => $item['level'],
+            'time'      => $item['time'],
+            'time2'      => $item['time2'],
+            'kind'      => $item['kind'],
+            'state'     => $item['state'],
+            'home'      => $item['home'],
             'home_name' => $home[0],
             'home_id'   => $home[3],
-            'away'      => $item['i'],
+            'away'      => $item['away'],
             'away_name' => $away[0],
             'away_id'   => $away[3],
-            'home_score' => $item['j'],
-            'away_score' => $item['k'],
-            'home_half_score' => $item['l'],
-            'away_half_score' => $item['m'],
-            'home_red'   => $item['n'],
-            'away_red'   => $item['o'],
-            'home_order' => $item['p'],
-            'away_order' => $item['q'],
-            'explain'    => $item['r'],
-            'match_round' => $item['s'],
-            'address' => $item['t'],
-            'weather_ico' => $item['u'],
-            'weather' => $item['v'],
-            'temperature' => $item['w'],
-            'match_league' => $item['x'],
-            'group' =>$item['y'],
-            'is_neutral' => $item['z']
+            'home_score' => $item['homeScore'],
+            'away_score' => $item['awayScore'],
+            'home_half_score' => $item['bc1'],
+            'away_half_score' => $item['bc2'],
+            'home_red'   => $item['red1'],
+            'away_red'   => $item['red2'],
+            'home_order' => $item['order1'],
+            'away_order' => $item['order2'],
+            'home_yellow' => $item['yellow1'],
+            'away_yellow' => $item['yellow2'],
+            'explain'    => $item['explain'],
+            'is_neutral' => $item['zl'],
+            'is_tv' => $item['tv'],
+            'is_lineup' => $item['lineup'],
+            'explain2' => $item['explain2']
         ];
         $team = $curr->findOne(array('match_id'=>$info['match_id']));
         if($team){
