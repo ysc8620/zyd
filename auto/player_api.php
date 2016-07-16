f43399fa4bc8fa2d96388e3ebdef56ea19040f4f
ShengYue <ysc8620@163.com>
2016-07-16 14:04:32 +0800

    ds

--- auto/player_api.php
+++ auto/player_api.php
@@ -17,13 +17,36 @@ require_once 'config.php';
 do{
     global $mongo;
     $curr = $mongo->zyd->player;
-    $postStr = file_get_contents("http://interface.win007.com/zq/Player_XML.aspx?day=1");
+    $postStr = file_get_contents("http://interface.win007.com/zq/Player_XML.aspx?day=60");
     $obj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
     $data = json_decode(json_encode($obj), true);
-    print_r($data);
-    die();
+
 
     foreach($data as $item){
+        $info = [
+            'from_id'   => $item['id'],
+            'player_id' => $item['PlayerID'],
+            'cn_name'   => $item['Name_J'],
+            'tw_name'   => $item['Name_F'],
+            'en_name'   => $item['Name_E'],
+            'birthday'  => $item['Birthday'],
+            'tallness'  => $item['Tallness'],
+            'weight'    => $item['Weight'],
+            'country'   => $item['Country'],
+            'photo'     => $item['Photo'],
+            'health'    => $item['Health'],
+            'value'     => $item['value'],
+            'feet'      => $item['feet'],
+            'introduce' => $item['Introduce'],
+            'team_id'  => $item['TeamID'],
+            'place'    => $item['Place'],
+            'number'   => $item['Number']
+        ];
         $player = $curr->findOne(array('player_id'=>$item['PlayerID']));
+        if($player){
+            $curr->update(array('player_id'=>$info['player_id']), array('$set'=>$info));
+        }else{
+            $curr->insert($info);
+        }
     }
 }while(false);
