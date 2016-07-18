6d844a0c97233dba63f94b16f2d8fe8bec1de50f
ysc8620 <ysc8620@163.com>
2016-07-18 23:03:24 +0800

    ds

--- Application/Admin/Controller/BasicController.class.php
+++ Application/Admin/Controller/BasicController.class.php
@@ -11,7 +11,11 @@ class BasicController extends BaseController {
         $curr = $mongo->zyd->league;
         $page = I('p',1,'intval');
         $total = $curr->count();
-        $list = $curr->find()->sort(array('update_time'=>-1))->skip( ($page-1)*20)->limit(20);
+        $obj = $curr->find()->sort(array('update_time'=>-1))->skip( ($page-1)*20)->limit(20);
+        $list = [];
+        while($row = $obj->hasNext()){
+            $list[] = $obj->getNext();
+        }
         print_r($list);
         $page = new \Think\Page($total, 20);
         $this->assign('page', $page->show());
