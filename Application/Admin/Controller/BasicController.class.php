518d331b20c693172438993b559b3448d94a65c5
ShengYue <ysc8620@163.com>
2016-07-19 17:51:12 +0800

    ds

--- Application/Admin/Controller/BasicController.class.php
+++ Application/Admin/Controller/BasicController.class.php
@@ -82,4 +82,78 @@ class BasicController extends BaseController {
 
         $this->display();
     }
+
+    public function player(){
+        $mongo  = $this->initMongo();
+        $curr = $mongo->zyd->player;
+        $page = I('p',1,'intval');
+        $total = $curr->count();
+        $obj = $curr->find()->sort(array('update_time'=>-1))->skip( ($page-1)*20)->limit(20);
+        $list = [];
+        while($row = $obj->hasNext()){
+            $list[] = $obj->getNext();
+        }
+        $page = new \Think\Page($total, 20);
+        $this->assign('page', $page->show());
+        $this->assign('list', $list);
+
+        $this->display();
+    }
+
+    /**
+     * 球队编辑
+     */
+    public function player_edit(){
+        $mongo = $this->initMongo();
+        $curr = $mongo->zyd->player;
+        $id = I('request.id',0,"intval");
+        if(empty($id)){
+            return $this->error('请选择球队', U('basic/team'));
+        }
+        if(IS_POST){
+
+        }
+
+        $team = $curr->findOne(array('team_id'=>$id));
+        $this->assign('team', $team);
+
+        $this->display();
+    }
+
+    public function referee(){
+        $mongo  = $this->initMongo();
+        $curr = $mongo->zyd->referee;
+        $page = I('p',1,'intval');
+        $total = $curr->count();
+        $obj = $curr->find()->sort(array('update_time'=>-1))->skip( ($page-1)*20)->limit(20);
+        $list = [];
+        while($row = $obj->hasNext()){
+            $list[] = $obj->getNext();
+        }
+        $page = new \Think\Page($total, 20);
+        $this->assign('page', $page->show());
+        $this->assign('list', $list);
+
+        $this->display();
+    }
+
+    /**
+     * 球队编辑
+     */
+    public function referee_edit(){
+        $mongo = $this->initMongo();
+        $curr = $mongo->zyd->referee;
+        $id = I('request.id',0,"intval");
+        if(empty($id)){
+            return $this->error('请选择球队', U('basic/team'));
+        }
+        if(IS_POST){
+
+        }
+
+        $team = $curr->findOne(array('team_id'=>$id));
+        $this->assign('team', $team);
+
+        $this->display();
+    }
 }
