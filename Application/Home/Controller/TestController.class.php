27804b888cfd55f95f9d754166ad206c9d1c3c75
ShengYue <ysc8620@163.com>
2016-07-21 12:20:11 +0800

    ds

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -13,8 +13,9 @@ class TestController extends BaseApiController {
     public function test(){
         $json = $this->simpleJson();
         $data = $_POST;
+        $json['post'] = $data;
         $json['data'] = ['id'=>1,'time'=>time(),'d'=>'返回中文测试'];
-        // $json['header'] = getallheaders();
+        $json['header'] = getallheaders();
         \Org\Util\File::write_file('./newpost.log', date("Y-m-d H:i:s")."==========================\r\n".json_encode($json)."\r\n==================================\r\rn");
         $this->ajaxReturn($json);
     }
