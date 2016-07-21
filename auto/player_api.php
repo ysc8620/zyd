5a123a422805261d3043079813bf1f4ad1325d82
ShengYue <ysc8620@163.com>
2016-07-21 17:27:31 +0800

    ds

--- auto/player_api.php
+++ auto/player_api.php
@@ -15,8 +15,8 @@ require_once __DIR__ .'/config.php';
 echo date("Y-m-d H:i:s")."=player_api=\r\n";
 //mongodb://admin_miss:miss@localhost:27017/test
 do{
-    global $mongo;
-    $curr = $mongo->zyd->player;
+//    global $mongo;
+//    $curr = $mongo->zyd->player;
     $postStr = file_get_contents("http://interface.win007.com/zq/Player_XML.aspx?day=1");
     $obj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
     $data = json_decode(json_encode($obj), true);
@@ -24,31 +24,31 @@ do{
 
     foreach($data['i'] as $item){
         $info = [
-            'from_id'   => $item['id'],
-            'player_id' => $item['PlayerID'],
-            'cn_name'   => $item['Name_J'],
-            'tw_name'   => $item['Name_F'],
-            'en_name'   => $item['Name_E'],
-            'birthday'  => $item['Birthday'],
-            'tallness'  => $item['Tallness'],
-            'weight'    => $item['Weight'],
-            'country'   => $item['Country'],
-            'photo'     => $item['Photo'],
-            'health'    => $item['Health'],
-            'value'     => $item['value'],
-            'feet'      => $item['feet'],
-            'introduce' => $item['Introduce'],
-            'team_id'  => $item['TeamID'],
-            'place'    => $item['Place'],
-            'number'   => $item['Number'],
+            'from_id'   => intval($item['id']),
+            'player_id' => intval($item['PlayerID']),
+            'cn_name'   => strval($item['Name_J']),
+            'tw_name'   => strval($item['Name_F']),
+            'en_name'   => strval($item['Name_E']),
+            'birthday'  => strval($item['Birthday']),
+            'tallness'  => strval($item['Tallness']),
+            'weight'    => strval($item['Weight']),
+            'country'   => strval($item['Country']),
+            'photo'     => strval($item['Photo']),
+            'health'    => strval($item['Health']),
+            'value'     => strval($item['value']),
+            'feet'      => strval($item['feet']),
+            'introduce' => strval($item['Introduce']),
+            'team_id'  => intval($item['TeamID']),
+            'place'    => strval($item['Place']),
+            'number'   => strval($item['Number']),
             'update_time' => time(),
             'update_date' => date('Y-m-d H:i:s')
         ];
-        $player = $curr->findOne(array('player_id'=>$item['PlayerID']));
+        $player = M('player')->where(array('player_id'=>$info['player_id']))->find();
         if($player){
-            $curr->update(array('player_id'=>$info['player_id']), array('$set'=>$info));
+            M('player')->where(array('player_id'=>$info['player_id']))->save($info);
         }else{
-            $curr->insert($info);
+            M('player')->add($info);
         }
     }
 }while(false);
