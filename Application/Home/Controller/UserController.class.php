76def275601dabb365dfd23e18809baa8d749597
ShengYue <ysc8620@163.com>
2016-07-25 18:24:13 +0800

    ds

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -3,6 +3,30 @@ namespace Home\Controller;
 
 class UserController extends BaseApiController {
     public $default_pic = '/Public/static/userimg.jpg';
+
+    private $field = "`id`, `nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`, `total_top_credit`,
+    `register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`,
+     `total_follow_user`,`total_rate`,`total_month_rate`,`password`,`salt`";
+
+    private function getField($type=''){
+        if($type){
+            $fields = explode(',', $this->field);
+            foreach($fields as $i=>$field){
+                $fields[$i] = $type.'.'.$field;
+            }
+            return join(',', $fields);
+        }else{
+            return $this->field;
+        }
+    }
+
+    private function get_return_member($member){
+        $member['pic'] = pic_url($member['pic']);
+        unset($member['status']);
+        unset($member['password']);
+        unset($member['salt']);
+        return $member;
+    }
     /**
      * 用户注册
      */
@@ -75,10 +99,10 @@ class UserController extends BaseApiController {
 
             $user_id = M('users')->add($user);
             if($user_id){
-                $member = M('users')->where(array('id'=>$user_id))->field("`id`, `nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`, `total_top_credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`")->find();
-                $member['pic'] = pic_url($member['pic']);
+                $member = M('users')->where(array('id'=>$user_id))->field($this->field)->find();
+
                 $json['msg'] = '用户注册成功';
-                $json['data'] = $member;
+                $json['data'] = $this->get_return_member($member);
             }else{
                 $json['status'] = 111;
                 $json['msg'] = '用户注册失败';
@@ -123,7 +147,7 @@ class UserController extends BaseApiController {
                         break;
                     }
                 }
-                $member = M('users')->where(array('mobile'=>$mobile))->field("`id`, `nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+                $member = M('users')->where(array('mobile'=>$mobile))->field($this->field)->find();
                 // 用户锁定
                 if(!$member){
                     $json['status'] = 111;
@@ -137,14 +161,13 @@ class UserController extends BaseApiController {
                     $json['msg'] = '用户被锁定不能使用';
                     break;
                 }
-                unset($member['status']);
-                $member['pic'] = pic_url($member['pic']);
+
                 $json['msg'] = '用户登录成功';
-                $json['data'] = $member;
+                $json['data'] = $this->get_return_member($member);
 
             // 密码登陆
             }elseif($password){
-                $member = M('users')->where(array('mobile'=>$mobile))->field("`id`,`password`,`salt`, `nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+                $member = M('users')->where(array('mobile'=>$mobile))->field($this->field)->find();
                 // 用户锁定
                 if(!$member){
                     $json['status'] = 111;
@@ -164,12 +187,9 @@ class UserController extends BaseApiController {
                     $json['msg'] = '登录信息错误';
                     break;
                 }
-                unset($member['status']);
-                unset($member['password']);
-                unset($member['salt']);
-                $member['pic'] = pic_url($member['pic']);
+
                 $json['msg'] = '用户登录成功';
-                $json['data'] = $member;
+                $json['data'] = $this->get_return_member($member);
             }else{
                 $json['status'] = 111;
                 $json['msg'] = '登录信息错误';
@@ -194,7 +214,7 @@ class UserController extends BaseApiController {
                 break;
             }
 
-            $member = M('users')->where(array('id'=>$user_id))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            $member = M('users')->where(array('id'=>$user_id))->field($this->field)->find();
             // 用户锁定
             if(!$member){
                 $json['status'] = 111;
@@ -208,9 +228,8 @@ class UserController extends BaseApiController {
                 $json['msg'] = '用户被锁定不能使用';
                 break;
             }
-            unset($member['status']);
-            $member['pic'] = pic_url($member['pic']);
-            $json['data'] = $member;
+
+            $json['data'] = $this->get_return_member($member);
 
         }while(false);
         $this->ajaxReturn($json);
@@ -232,7 +251,7 @@ class UserController extends BaseApiController {
                 $json['msg'] = '微信授权ID不能为空';
                 break;
             }
-            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field($this->field)->find();
             if($wx_user){
                 if($wx_user['status'] < 1){
                     $json['status'] = 111;
@@ -240,7 +259,7 @@ class UserController extends BaseApiController {
                     break;
                 }
                 $json['msg'] = '微信登录成功';
-                $json['data'] = $wx_user;
+                $json['data'] = $this->get_return_member($wx_user);
                 break;
             }else{
                 $data = [
@@ -253,8 +272,9 @@ class UserController extends BaseApiController {
                 ];
                 $res = M('users')->add($data);
                 if($res){
+                    $member = M('users')->where(array('wx_openid'=>$openid))->field($this->field)->find();
                     $json['msg'] = '微信登录成功';
-                    $json['data'] = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+                    $json['data'] = $this->get_return_member($member);
                     break;
                 }else{
                     $json['status'] = 111;
@@ -294,12 +314,11 @@ class UserController extends BaseApiController {
                 break;
             }
 
-            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field($this->field)->find();
             if($wx_user){
                 if($wx_user['id'] == $user_id){
-                    unset($wx_user['status']);
                     $json['msg'] = '微信绑定成功';
-                    $json['data'] = $wx_user;
+                    $json['data'] = $this->get_return_member($wx_user);
                     break;
                 }else{
                     $json['status'] = 111;
@@ -320,10 +339,9 @@ class UserController extends BaseApiController {
                 }
                 $res = M('users')->where(array('id'=>$user_id))->save($data);
                 if($res){
-                    $member = M('users')->where(array('id'=>$user_id))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
-                    unset($member['status']);
+                    $member = M('users')->where(array('id'=>$user_id))->field($this->field)->find();
                     $json['msg'] = '微信绑定成功';
-                    $json['data'] = $member;
+                    $json['data'] = $this->get_return_member($member);
                     break;
                 }else{
                     $json['status'] = 111;
@@ -350,8 +368,7 @@ class UserController extends BaseApiController {
                 $json['msg'] = '用户昵称在1-36个字符之间';
                 break;
             }
-            $member = M('users')->where(array('id'=>$user_id))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
-            unset($member['status']);;
+            $member = M('users')->where(array('id'=>$user_id))->field($this->field)->find();
             if(!$member){
                 $json['status'] = 111;
                 $json['msg'] = '没找到用户信息';
@@ -364,7 +381,7 @@ class UserController extends BaseApiController {
             $res = M('users')->where(array('id'=>$user_id))->save($data);
             if($res){
                 $member['nickname'] = $nickname;
-                $json['data'] = $member;
+                $json['data'] = $this->get_return_member($member);
                 $json['msg'] = '用户编辑成功';
                 break;
             }else{
@@ -438,9 +455,11 @@ class UserController extends BaseApiController {
             $Page->show();
 
             $list = M()->table(C('DB_PREFIX').'users as u, '.C('DB_PREFIX').'users_follow as uf')->where("u.status = 1 AND uf.from_user_id=$user_id AND u.id = uf.from_user_id")
-                ->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`")
+                ->field($this->getField('u'))
                 ->limit($Page->firstRow . ',' . $Page->listRows)->order("uf.create_time DESC")->select();
-            
+            foreach($list as $i=>$item){
+                $list[$i] = $this->get_return_member($item);
+            }
             $json['data'] = [
                 'list' => $list,
                 'page' => $page,
@@ -449,6 +468,82 @@ class UserController extends BaseApiController {
                 'total_page' => ceil($total/$limit),
                 'user_id' => $user_id
             ];
+        }while(false);
+        $this->ajaxReturn($json);
+    }
+
+    /**
+     * 获取专家列表
+     */
+    public function get_expert_list(){
+        $json = $this->simpleJson();
+        do {
+            // 1 注册, 2登录, 3,找回密码
+            $user_id = I('request.user_id', 0, 'intval');
+            $limit = I('request.limit',10,'intval');
+            $page = I('request.p',1,'intval');
+            $type = I('request.type',1,'intval'); // 专家类型： 1 按30天胜率， 2，按关注数， 3, 我关注的
+            if($type == 1){
+                $total = M('users')->where("is_expert=1 AND status = 1")->count();
+                $Page = new \Think\Page($total, $limit); // 实例化分页类 传入总记录数和每页显示的记录数(25)
+                $Page->show();
+
+                $list = M('users')->where("is_expert=1 AND status = 1")
+                    ->field($this->field)
+                    ->limit($Page->firstRow . ',' . $Page->listRows)->order("total_follow_user DESC, total_rate DESC")->select();
+
+                $json['data'] = [
+                    'list' => $list,
+                    'page' => $page,
+                    'total' => $total,
+                    'limit' => $limit,
+                    'total_page' => ceil($total/$limit),
+                    'user_id' => $user_id,
+                    'type' => $type
+                ];
+            }elseif($type == 2){
+                $total = M('users')->where("is_expert=1 AND status = 1")->count();
+                $Page = new \Think\Page($total, $limit); // 实例化分页类 传入总记录数和每页显示的记录数(25)
+                $Page->show();
+
+                $list = M('users')->where("is_expert=1 AND status = 1")
+                    ->field($this->field)
+                    ->limit($Page->firstRow . ',' . $Page->listRows)->order("total_month_rate DESC, total_rate DESC")->select();
+
+                $json['data'] = [
+                    'list' => $list,
+                    'page' => $page,
+                    'total' => $total,
+                    'limit' => $limit,
+                    'total_page' => ceil($total/$limit),
+                    'user_id' => $user_id,
+                    'type' => $type
+                ];
+            }elseif($type == 3){
+                if(empty($user_id)){
+                    $json['status'] = 101;
+                    $json['msg'] = '请先登录';
+                    break;
+                }
+
+                $total = M()->table(C('DB_PREFIX').'users as u, '.C('DB_PREFIX').'users_follow as uf')->where("u.is_expert=1 AND u.status = 1 AND uf.from_user_id=$user_id AND u.id = uf.from_user_id")->count();
+                $Page = new \Think\Page($total, $limit); // 实例化分页类 传入总记录数和每页显示的记录数(25)
+                $Page->show();
+
+                $list = M()->table(C('DB_PREFIX').'users as u, '.C('DB_PREFIX').'users_follow as uf')->where("u.is_expert=1 AND u.status = 1 AND uf.from_user_id=$user_id AND u.id = uf.from_user_id")
+                    ->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`")
+                    ->limit($Page->firstRow . ',' . $Page->listRows)->order("uf.create_time DESC")->select();
+
+                $json['data'] = [
+                    'list' => $list,
+                    'page' => $page,
+                    'total' => $total,
+                    'limit' => $limit,
+                    'total_page' => ceil($total/$limit),
+                    'user_id' => $user_id,
+                    'type' => $type
+                ];
+            }
         }while(false);
         $this->ajaxReturn($json);
     }
