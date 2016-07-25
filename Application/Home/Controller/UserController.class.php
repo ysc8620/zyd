6f217d39534adc3b13a611ddb9abc452c63400a6
ShengYue <ysc8620@163.com>
2016-07-25 17:40:58 +0800

    ds

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -2,7 +2,7 @@
 namespace Home\Controller;
 
 class UserController extends BaseApiController {
-
+    public $default_pic = '/Public/static/userimg.jpg';
     /**
      * 用户注册
      */
@@ -51,7 +51,7 @@ class UserController extends BaseApiController {
             $user = [
                 'nickname' => $mobile,
                 'mobile' => $mobile,
-                'pic' => '/Public/static/userimg.jpg'
+                'pic' => $this->default_pic
             ];
 
             if($password){
@@ -216,4 +216,163 @@ class UserController extends BaseApiController {
         $this->ajaxReturn($json);
     }
 
+    /**
+     * 微信登录
+     */
+    public function wx_login(){
+        $json = $this->simpleJson();
+        do {
+            // 1 注册, 2登录, 3,找回密码
+            $openid = I('request.openid','','strval');
+            $nickname = I('request.nickname','','strval');
+            $pic = I('request.pic','','strval');
+
+            if(empty($openid)){
+                $json['status'] = 110;
+                $json['msg'] = '微信授权ID不能为空';
+                break;
+            }
+            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            if($wx_user){
+                if($wx_user['status'] < 1){
+                    $json['status'] = 111;
+                    $json['msg'] = '用户被锁定不能使用';
+                    break;
+                }
+                $json['msg'] = '微信登录成功';
+                $json['data'] = $wx_user;
+                break;
+            }else{
+                $data = [
+                    'nickname' => $nickname,
+                    'pic' => $pic,
+                    'mobile' => '',
+                    'wx_openid' => $openid,
+                    'register_time' => time(),
+                    'update_time' => time()
+                ];
+                $res = M('users')->add($data);
+                if($res){
+                    $json['msg'] = '微信登录成功';
+                    $json['data'] = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+                    break;
+                }else{
+                    $json['status'] = 111;
+                    $json['msg'] = '微信登录失败';
+                    break;
+                }
+            }
+        }while(false);
+    }
+
+    /**
+     * 微信用户绑定或登录
+     */
+    public function wx_bind(){
+        $json = $this->simpleJson();
+        do {
+            // 1 注册, 2登录, 3,找回密码
+            $user_id = I('request.user_id', 0, 'intval');
+            $openid = I('request.openid','','strval');
+            $nickname = I('request.nickname','','strval');
+            $pic = I('request.pic','','strval');
+            if(empty($openid)){
+                $json['status'] = 110;
+                $json['msg'] = '微信授权ID不能为空';
+                break;
+            }
+
+            if(empty($user_id)){
+                $json['status'] = 110;
+                $json['msg'] = '绑定用户ID不能为空';
+                break;
+            }
+            $member = M('users')->where(array('id'=>$user_id))->find();
+            if(!$member){
+                $json['status'] = 111;
+                $json['msg'] = '找不到绑定用户信息';
+                break;
+            }
+
+            $wx_user = M('users')->where(array('wx_openid'=>$openid))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            if($wx_user){
+                if($wx_user['id'] == $user_id){
+                    unset($wx_user['status']);
+                    $json['msg'] = '微信绑定成功';
+                    $json['data'] = $wx_user;
+                    break;
+                }else{
+                    $json['status'] = 111;
+                    $json['msg'] = '微信已绑定其他用户';
+                    $json['data'] = $wx_user;
+                    break;
+                }
+            }else{
+                $data = [
+                    'wx_openid' => $openid,
+                    'update_time' => time()
+                ];
+                if($member['nickname'] == $member['mobile'] && $nickname){
+                    $data['nickname'] = $nickname;
+                }
+                if($member['pic'] == $this->default_pic){
+                    $member['pic'] = $pic;
+                }
+                $res = M('users')->where(array('id'=>$user_id))->save($data);
+                if($res){
+                    $member = M('users')->where(array('id'=>$user_id))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+                    unset($member['status']);
+                    $json['msg'] = '微信绑定成功';
+                    $json['data'] = $member;
+                    break;
+                }else{
+                    $json['status'] = 111;
+                    $json['msg'] = '微信绑定失败';
+                    break;
+                }
+            }
+        }while(false);
+        $this->ajaxReturn($json);
+    }
+
+    /**
+     * 用户编辑
+     */
+    public function edit_user(){
+        $json = $this->simpleJson();
+        do {
+            // 1 注册, 2登录, 3,找回密码
+            $user_id = I('request.user_id', 0, 'intval');
+            $nickname = I('request.nickname','','trim,strval,htmlspecialchars,strip_tags');
+
+            if(empty($nickname) || strlen($nickname) > 36){
+                $json['status'] = 110;
+                $json['msg'] = '用户昵称在1-36个字符之间';
+                break;
+            }
+            $member = M('users')->where(array('id'=>$user_id))->field("`id`,`nickname`, `pic`, `mobile`, `is_expert`, `vip`, `credit`,`register_time`, `update_time`,  `total_send_info`, `total_collect_user`, `total_collect_match`, `total_follow_user`,`status`")->find();
+            unset($member['status']);;
+            if(!$member){
+                $json['status'] = 111;
+                $json['msg'] = '没找到用户信息';
+                break;
+            }
+            $data = [
+                'nickname' => $nickname,
+                'update_time' => time()
+            ];
+            $res = M('users')->where(array('id'=>$user_id))->save($data);
+            if($res){
+                $member['nickname'] = $nickname;
+                $json['data'] = $member;
+                $json['msg'] = '用户编辑成功';
+                break;
+            }else{
+                $json['status'] = 111;
+                $json['msg'] = '用户编辑失败';
+                break;
+            }
+        }while(false);
+        $this->ajaxReturn($json);
+    }
 }
