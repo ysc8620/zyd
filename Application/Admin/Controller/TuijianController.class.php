1c65eedc9eec72b7e75f39e59a7c5152bae0dfc4
ShengYue <ysc8620@163.com>
2016-07-27 16:31:59 +0800

    ds

--- Application/Admin/Controller/TuijianController.class.php
+++ Application/Admin/Controller/TuijianController.class.php
@@ -2,10 +2,10 @@
 namespace Admin\Controller;
 use Think\Controller;
 use Think\Page;
-class MatchController extends BaseController {
+class TuijianController extends BaseController {
     public function index(){
         $state = I('status','','strval');
-        $match = M('match'); // 实例化User对象
+        $match = M('tuijian'); // 实例化User对象
         if($state !== ''){
             $match->where(array('state'=>$state));
         }
@@ -18,7 +18,7 @@ class MatchController extends BaseController {
             $match->where(array('state'=>$state));
         }
         $this->assign('state', $state);
-        $list = $match->order('time DESC')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+        $list = $match->order('create_time DESC')->limit($Page->firstRow . ',' . $Page->listRows)->select();
         $this->assign('list', $list);// 赋值数据集
         $this->assign('page', $show);// 赋值分页输出
         $this->display();
@@ -27,9 +27,8 @@ class MatchController extends BaseController {
     /**
      * 赛事关注列表
      */
-    public function follow(){
-
-        $match = M('match_follow'); // 实例化User对象
+    public function pay(){
+        $match = M('tuijian_order'); // 实例化User对象
 
         $count = $match->count();// 查询满足要求的总记录数
         $Page = new Page($count, 20);// 实例化分页类 传入总记录数和每页显示的记录数(25)
