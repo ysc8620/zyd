99a973f00dd868fa1e4bc312949361d798b382df
ShengYue <ysc8620@163.com>
2016-07-27 17:36:54 +0800

    ds

--- Application/Admin/Controller/UsersController.class.php
+++ Application/Admin/Controller/UsersController.class.php
@@ -18,7 +18,7 @@ class UsersController extends BaseController
 
         $show = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $list = $city->order('create_time')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+        $list = $city->order('register_time')->limit($Page->firstRow . ',' . $Page->listRows)->select();
         $this->assign('list', $list);// 赋值数据集
         $this->assign('page', $show);// 赋值分页输出
         $this->display();
