bd420e1cec842f9958bd937d85051c12667499ae
ysc8620 <ysc8620@163.com>
2016-08-07 11:36:34 +0800

    ds

--- Application/Home/Controller/BaseApiController.class.php
+++ Application/Home/Controller/BaseApiController.class.php
@@ -124,8 +124,39 @@ class BaseApiController extends BaseController {
             'status' => 100,
             'msg'  => '',
             'time' => time(),
-            'data' => ''
+            'data' => []
         ];
+    }
+
+    protected function ajaxReturn($data,$type='',$json_option=0) {
+        if(empty($type)) $type  =   C('DEFAULT_AJAX_RETURN');
+        if(isset($data['data'])){
+            if(empty($data['data'])){
+                $data['data'] = (object)$data['data'];
+            }
+        }
+        switch (strtoupper($type)){
+            case 'JSON' :
+                // 返回JSON数据格式到客户端 包含状态信息
+                header('Content-Type:application/json; charset=utf-8');
+                exit(json_encode($data,$json_option));
+            case 'XML'  :
+                // 返回xml格式数据
+                header('Content-Type:text/xml; charset=utf-8');
+                exit(xml_encode($data));
+            case 'JSONP':
+                // 返回JSON数据格式到客户端 包含状态信息
+                header('Content-Type:application/json; charset=utf-8');
+                $handler  =   isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
+                exit($handler.'('.json_encode($data,$json_option).');');
+            case 'EVAL' :
+                // 返回可执行的js脚本
+                header('Content-Type:text/html; charset=utf-8');
+                exit($data);
+            default     :
+                // 用于扩展其他返回格式数据
+                Hook::listen('ajax_return',$data);
+        }
     }
 
     public function initMongo(){
