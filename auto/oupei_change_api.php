482ea15e4384f09640ce76cf5c6a244b079fcf39
ysc8620 <ysc8620@163.com>
2016-08-09 21:54:08 +0800

    ds

--- auto/oupei_change_api.php
+++ auto/oupei_change_api.php
@@ -25,7 +25,8 @@ do{
     $list = json_decode(json_encode($obj), true);
     foreach($list['h'] as $match){
         if(is_array($match['odds']['o'])){
-
+            echo '22';
+            print_r($match['odds']);
             foreach($match['odds']['o'] as $rate){
                 $item = explode($rate,',');
                 //`match_id`, `company_id`, `begin_home_rate`, `begin_draw_rate`, `begin_away_rate`, `change_home_rate`,
@@ -49,6 +50,8 @@ do{
             }
         }else{
             $item = explode($match['odds']['o'],',');
+            echo '11';
+            print_r($match['odds']['o']);
             //`match_id`, `company_id`, `begin_home_rate`, `begin_draw_rate`, `begin_away_rate`, `change_home_rate`,
             // `change_draw_rate`, `change_away_rate`, `change_date`, `update_time`
             $data = [
