8cde6622eddd75afb6a39b66512901e56c7750a6
ShengYue <ysc8620@163.com>
2016-08-12 14:18:45 +0800

    ds

--- log.php
+++ log.php
@@ -68,7 +68,7 @@ $data = [
 
 
 //$data['apple_receipt'] = $apple_receipt;
-$result = httpPost("https://api.zydzuqiu.com/pay/api.html", $data);
+$result = httpPost("https://api.zydzuqiu.com/api/notify/type/alipay.html", $data);
 //var_dump($result);
 echo ($result);
 print_r(json_decode($result, true));
