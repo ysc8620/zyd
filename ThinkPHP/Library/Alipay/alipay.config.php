1febf04c58c017c23d40d8c1f191cd223a4aa696
ShengYue <ysc8620@163.com>
2016-08-12 13:58:56 +0800

    ds

--- ThinkPHP/Library/Alipay/alipay.config.php
+++ ThinkPHP/Library/Alipay/alipay.config.php
@@ -13,7 +13,8 @@
 $alipay_config['partner']		= '2088421319080851';
 
 //商户的私钥,此处填写原始私钥去头去尾，RSA公私钥生成：https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
-$alipay_config['private_key']	= 'MIICXAIBAAKBgQDLuU07i+5NOapBuGY8sJyj9pXSM+8ataQNJTNkaQVGCfirRDL6
+$alipay_config['private_key']	= '-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQDLuU07i+5NOapBuGY8sJyj9pXSM+8ataQNJTNkaQVGCfirRDL6
 7HdUteMUj4fOJqCnBSPj73FV+h1PZOTE3sztzguKCRbJBBca0IAOPY5mg+zQHQoP
 nvXyFL05axWbvbG4Pg6Z7oeCvJedFEvAHIY4iA+eGBc4Zq0vDIq1+3UgqwIDAQAB
 AoGAauibNxx/udcBGewC+uZTzVeh1+WZHJMlvCBBj/fs5IxnCrmHZ1agaLvqpueL
@@ -25,7 +26,8 @@ X8CNT/Kg4+QpOif7sh4+zjr0NpE8pJnszQJAG7DylbW2xsoSA+lb9jnBM6oUNqrs
 FzTHr5wUtp+rS3nN5CzOmAeIt+esEB6hO9ZgPl7ty7IIb47DSfmfU7jLWQJBAJEr
 Ezcz1h7uVKXMsAQ0K3j5eefrrD+hhBEh7i4g+nBAU8xbmoQR2XiHwVJiZao1qxfi
 wxfh28Jyc0x8vuoNx+0CQGivPqksmBD6kWtiip8d5KOEGdtf1gh4z0TIETuKHiov
-70wXdtUv4deKus98cbnldO1PFqVbn6QYy/MT8IRHe+w=';
+70wXdtUv4deKus98cbnldO1PFqVbn6QYy/MT8IRHe+w=
+-----END RSA PRIVATE KEY-----';
 
 //支付宝的公钥，查看地址：https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 $alipay_config['alipay_public_key']= 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB';
