df74e12738ead5a7f307adea0c5c5c44702b29e4
ysc8620 <ysc8620@163.com>
2016-08-16 22:58:04 +0800

    ds

--- Application/Home/Controller/MatchController.class.php
+++ Application/Home/Controller/MatchController.class.php
@@ -150,12 +150,12 @@ class MatchController extends BaseApiController {
 
                 // 竟彩
                 $jingcai = get_rate($match['match_id'],'jingcai',$match['state']);
-                $list[$i]['jingcai']['home_rate'] = $jingcai['begin_rate'];
-                $list[$i]['jingcai']['away_rate'] = $jingcai['begin_big_rate'];
-                $list[$i]['jingcai']['draw_rate'] = $jingcai['begin_small_rate'];
-                $list[$i]['jingcai']['home_win_rate'] = $jingcai['change_rate'];
-                $list[$i]['jingcai']['away_win_rate'] = $jingcai['change_big_rate'];
-                $list[$i]['jingcai']['draw_win_rate'] = $jingcai['change_small_rate'];
+                $list[$i]['jingcai']['home_rate'] = $jingcai['home_rate'];
+                $list[$i]['jingcai']['away_rate'] = $jingcai['away_rate'];
+                $list[$i]['jingcai']['draw_rate'] = $jingcai['draw_rate'];
+                $list[$i]['jingcai']['home_win_rate'] = $jingcai['home_win_rate'];
+                $list[$i]['jingcai']['away_win_rate'] = $jingcai['away_win_rate'];
+                $list[$i]['jingcai']['draw_win_rate'] = $jingcai['draw_win_rate'];
 
                 // 直播事件
                 $event_list = M('event')->where(array('match_id'=>$match['match_id'], 'event_type'=>1))->order("time DESC")->select();
@@ -308,12 +308,12 @@ class MatchController extends BaseApiController {
 
             // 竟彩
             $jingcai = get_rate($match['match_id'],'jingcai',$match['state']);
-            $match['jingcai']['home_rate'] = $jingcai['begin_rate'];
-            $match['jingcai']['away_rate'] = $jingcai['begin_big_rate'];
-            $match['jingcai']['draw_rate'] = $jingcai['begin_small_rate'];
-            $match['jingcai']['home_win_rate'] = $jingcai['change_rate'];
-            $match['jingcai']['away_win_rate'] = $jingcai['change_big_rate'];
-            $match['jingcai']['draw_win_rate'] = $jingcai['change_small_rate'];
+            $match['jingcai']['home_rate'] = $jingcai['home_rate'];
+            $match['jingcai']['away_rate'] = $jingcai['away_rate'];
+            $match['jingcai']['draw_rate'] = $jingcai['draw_rate'];
+            $match['jingcai']['home_win_rate'] = $jingcai['home_win_rate'];
+            $match['jingcai']['away_win_rate'] = $jingcai['away_win_rate'];
+            $match['jingcai']['draw_win_rate'] = $jingcai['draw_win_rate'];
 
             // 直播事件
             $event_list = M('event')->where(array('match_id'=>$match['match_id']))->order("time ASC")->select();
@@ -387,10 +387,10 @@ class MatchController extends BaseApiController {
 
             // 竞彩
             $jingcai = get_rate($match['match_id'],'jingcai',$match['state']);
-            $tuijian['jingcai'] = ["rate_1"=>$jingcai['change_home_rate'], "rate_2"=>$jingcai['change_draw_rate'], "rate_3"=>$baiou['change_away_rate']];
+            $tuijian['jingcai'] = ["rate_1"=>$jingcai['home_win_rate'], "rate_2"=>$jingcai['draw_win_rate'], "rate_3"=>$baiou['away_win_rate']];
             // 竞彩让球
             $jingcai = get_rate($match['match_id'],'jingcai_rangqiu',$match['state']);
-            $tuijian['jingcai_rangqiu'] = ["rate_4"=>$jingcai['change_home_rate'], "rate_5"=>$jingcai['change_draw_rate'], "rate_6"=>$baiou['change_away_rate']];
+            $tuijian['jingcai_rangqiu'] = ["rate_4"=>$jingcai['home_win_rate'], "rate_5"=>$jingcai['draw_win_rate'], "rate_6"=>$baiou['away_win_rate']];
             // 亚赔
             $yapei = get_rate($match['match_id'],'rangqiu',$match['state']);//`change_rate`, `change_home_rate`, `change_away_rate`
             $tuijian['rangqiu'] = ["rate_1"=>$yapei['change_home_rate'], "rate_2"=>$yapei['change_rate'], "rate_3"=>$yapei['change_away_rate']];
