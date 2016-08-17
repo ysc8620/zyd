f3fe28ea80f4b296449fd8feb0a9325e7a7e0008
ysc8620 <ysc8620@163.com>
2016-08-17 22:54:46 +0800

    ds

--- Application/Home/Controller/MatchController.class.php
+++ Application/Home/Controller/MatchController.class.php
@@ -46,7 +46,7 @@ class MatchController extends BaseApiController {
                 $list = M('match')->field
                 ('match_id,time as match_time,league_id,league_name,kind,level,state,home_id,home_name,home_score,away_id,
                 away_name,away_score,home_red,away_red,home_yellow,away_yellow,match_round,address,weather_ico,weather,temperature,is_neutral,technic,total_collect'
-                )->where($where)->order("time DESC")->select();
+                )->where($where)->order("time ASC")->select();
 
             }elseif($type == 2){
 
