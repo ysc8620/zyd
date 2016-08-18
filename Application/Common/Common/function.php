68e85870ada0c763bb4239c9eaf9c871bf788123
ysc8620 <ysc8620@163.com>
2016-08-18 19:35:01 +0800

    live change time

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -626,6 +626,9 @@ function get_rate_list($match_id){
             }
         }
     }
+    foreach($json['oupei'] as $i=>$item){
+        $json['oupei'][$i]['change_date'] = date("H:i",strtotime($item['change_date']));
+    }
 
     // 变盘数据
     $field2 = " `match_id`, '' as time, '' as home_score, '' as away_score, `company_id`, change_home_rate as  rate_1, change_draw_rate as rate_2, change_away_rate as rate_3, '' as change_date";
@@ -669,6 +672,9 @@ function get_rate_list($match_id){
             }
         }
     }
+    foreach($json['rangqiu'] as $i=>$item){
+        $json['rangqiu'][$i]['change_date'] = date("H:i",strtotime($item['change_date']));
+    }
 
     // 变盘数据
     $field2 = " `match_id`, '' as time, '' as home_score, '' as away_score, `company_id`, change_home_rate as  rate_1, change_rate as rate_2, change_away_rate as rate_3, '' as change_date";
@@ -711,6 +717,9 @@ function get_rate_list($match_id){
                 }
             }
         }
+    }
+    foreach($json['daxiaoqiu'] as $i=>$item){
+        $json['daxiaoqiu'][$i]['change_date'] = date("H:i",strtotime($item['change_date']));
     }
 
     // 变盘数据
