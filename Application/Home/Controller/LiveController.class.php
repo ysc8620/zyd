8260ed1556e1d4e6fedf1ebaec8b3f6934fcac5b
ysc8620 <ysc8620@163.com>
2016-08-23 14:26:16 +0800

    ds

--- Application/Home/Controller/LiveController.class.php
+++ Application/Home/Controller/LiveController.class.php
@@ -7,18 +7,180 @@ use Redis\MyRedis;
 
 
 class LiveController extends BaseApiController {
+    public function index2(){
+        $json = $this->simpleJson();
+        do{
+            $league_idsstr = I('request.league_ids','','strval');
+            $where = [];
+            $user_id = intval($this->user['id']);
+
+            $league_ids_list = explode(',',$league_idsstr);
+            $league_ids = [];
+            foreach($league_ids_list as $league_id){
+                if(empty($league_id)){continue;}
+                $league_ids[] = $league_id;
+            }
+
+            // 进行中
+            $where['state'] = array('in',array(1,2,3,4));
+            if($league_ids){
+                $where['league_id'] = array('in', $league_ids);
+            }
+            $total = M('match')->where($where)->count();
+            $list = M('match')->field
+            ('match_id,time as match_time,league_id,league_name,kind,level,state,home_id,home_name,home_score,away_id,
+            away_name,away_score,home_red,away_red,home_yellow,away_yellow,match_round,address,weather_ico,weather,temperature,is_neutral,technic,total_collect,total_tuijian'
+            )->where($where)->order("time ASC")->select();
+
+            foreach($list as $i=>$match){
+                // 赛事统计
+                $technic = empty($match['technic'])?[]:json_decode($match['technic'], true);
+                $list[$i]['technic'] = [];
+
+                $list[$i]['technic']['id3']['home'] = isset($technic['id3'])?($technic['id3']['home']):"0";
+                $list[$i]['technic']['id3']['away'] = isset($technic['id3'])?($technic['id3']['away']):"0";
+                $list[$i]['technic']['id4']['home'] = isset($technic['id4'])?($technic['id4']['home']):"0";
+                $list[$i]['technic']['id4']['away'] = isset($technic['id4'])?($technic['id4']['away']):"0";
+                $list[$i]['technic']['id6']['home'] = isset($technic['id6'])?($technic['id6']['home']):"0";
+                $list[$i]['technic']['id6']['away'] = isset($technic['id6'])?($technic['id6']['away']):"0";
+                $list[$i]['technic']['id14']['home'] = isset($technic['id14'])?($technic['id14']['home']):"0";
+                $list[$i]['technic']['id14']['away'] = isset($technic['id14'])?($technic['id14']['away']):"0";
+
+                // 亚赔
+                $yapei = get_rate($match['match_id'],'rangqiu',$match['state']);
+                $list[$i]['yapei']['begin_rate'] = $yapei['begin_rate'];
+                $list[$i]['yapei']['begin_home_rate'] = $yapei['begin_home_rate'];
+                $list[$i]['yapei']['begin_away_rate'] = $yapei['begin_away_rate'];
+                $list[$i]['yapei']['change_rate'] = $yapei['change_rate'];
+                $list[$i]['yapei']['change_home_rate'] = $yapei['change_home_rate'];
+                $list[$i]['yapei']['change_away_rate'] = $yapei['change_away_rate'];
+
+                // 标准
+                $biaozhun = get_rate($match['match_id'],'oupei',$match['state']);
+                $list[$i]['begin_rate'] = $yapei['begin_rate'];
+                $list[$i]['begin_home_rate'] = $yapei['begin_home_rate'];
+                $list[$i]['begin_away_rate'] = $yapei['begin_away_rate'];
+                $list[$i]['change_rate'] = $yapei['change_rate'];
+                $list[$i]['change_home_rate'] = $yapei['change_home_rate'];
+                $list[$i]['change_away_rate'] = $yapei['change_away_rate'];
+
+                // 欧赔
+                $list[$i]['oupei']['begin_home_rate'] =  $biaozhun['begin_home_rate'];
+                $list[$i]['oupei']['begin_draw_rate'] = $biaozhun['begin_draw_rate'];
+                $list[$i]['oupei']['begin_away_rate'] = $biaozhun['begin_away_rate'];
+                $list[$i]['oupei']['change_home_rate'] = $biaozhun['change_home_rate'];
+                $list[$i]['oupei']['change_draw_rate'] = $biaozhun['change_draw_rate'];
+                $list[$i]['oupei']['change_away_rate'] = $biaozhun['change_away_rate'];
+
+                // 大小球
+                $daxiaoqiu = get_rate($match['match_id'],'daxiaoqiu',$match['state']);
+                $list[$i]['daxiaoqiu']['begin_rate'] = $daxiaoqiu['begin_rate'];
+                $list[$i]['daxiaoqiu']['begin_big_rate'] = $daxiaoqiu['begin_big_rate'];
+                $list[$i]['daxiaoqiu']['begin_small_rate'] = $daxiaoqiu['begin_small_rate'];
+                $list[$i]['daxiaoqiu']['change_rate'] = $daxiaoqiu['change_rate'];
+                $list[$i]['daxiaoqiu']['change_big_rate'] = $daxiaoqiu['change_big_rate'];
+                $list[$i]['daxiaoqiu']['change_small_rate'] = $daxiaoqiu['change_small_rate'];
+
+                // 竟彩
+                $jingcai = get_rate($match['match_id'],'jingcai',$match['state']);
+                $list[$i]['jingcai']['home_rate'] = $jingcai['home_rate'];
+                $list[$i]['jingcai']['away_rate'] = $jingcai['away_rate'];
+                $list[$i]['jingcai']['draw_rate'] = $jingcai['draw_rate'];
+                $list[$i]['jingcai']['home_win_rate'] = $jingcai['home_win_rate'];
+                $list[$i]['jingcai']['away_win_rate'] = $jingcai['away_win_rate'];
+                $list[$i]['jingcai']['draw_win_rate'] = $jingcai['draw_win_rate'];
+
+                // 直播事件
+                $event_list = M('event')->where(array('match_id'=>$match['match_id'], 'event_type'=>1))->order("time DESC")->select();
+                $list[$i]['events'] = (array)$event_list;
+                $jingcai_info = M('jingcai')->where(array('match_id'=>$match['match_id']))->find();
+                $match_name = "";
+                if($jingcai_info){
+                    $match_name = getWeekName($jingcai_info['date']).$jingcai_info['match_no'];
+                }
+                $list[$i]['match_name'] = $match_name;
+
+                $zoudi = M('zoudi')->where(array('match_id'=>$match['match_id']))->order("id DESC")->find();
+                if(!$zoudi){
+                    unset($list[$i]);
+                    continue;
+                }
+                $zoudi['time'] = str_replace('分','',$zoudi['time']);
+
+                $list[$i]['match_time2'] = is_numeric($zoudi['time'])?$zoudi['time']."'":$zoudi['time'];
+                if($zoudi['time'] == '全场'){
+                    $list[$i]['match_time2'] = -1;
+                }
+
+                $list[$i]['state_name'] = getMatchStatus($match['state']);
+
+                //
+                $list[$i]['is_collect'] = 0;
+                if($user_id){
+                    $collect = M('match_follow')->where(array('user_id'=>$user_id, 'match_id'=>$match['match_id']))->find();
+                    if($collect){
+                        $list[$i]['is_collect'] = 1;
+                    }
+                }
+            }
+
+            $data = [];
+            foreach($list as $match){
+                if(empty($match)){continue;}
+                $key = 'i'.date("dHi",strtotime($match['match_time'])).$match['league_id'];
+                $data[$key]['league_id'] = $match['league_id'];
+                $data[$key]['league_name'] = $match['league_name'];
+                $data[$key]['league_ico'] = C('BASE_URL').'Public/static/noimg.png';
+                $data[$key]['list'][] = $match;
+            }
+            // $json['ngss'] = $data;
+            $newdata = [];
+            foreach($data as $item){
+                $newdata[] = $item;
+            }
+
+            $league_list = M()->table(C('DB_PREFIX').'league as l, '.C('DB_PREFIX').'match as m')->where("l.league_id = m.league_id AND m.state in(1,2,3,4)")
+                ->field('l.league_id,l.color,l.cn_short,l.cn_name,l.type,l.sum_round,l.curr_round,l.curr_match_season,l.country_name,l.is_hot,count(*) as total_match')
+                ->group('m.league_id')
+                ->order("is_hot DESC, weight DESC")->select();
+
+            foreach($league_list as $i=>$league){
+                $league_list[$i]['index_name'] =Getzimu($league['cn_name']);
+            }
+
+            $json['data']['league_list'] = (array)$league_list;
+            $json['data']['list'] = (array)$newdata;
+            $json['data']['total'] = $total;
+            $json['data']['league_ids'] = $league_idsstr;
+
+        }while(false);
+        $this->ajaxReturn($json);
+    }
+
+
     /**
      * 返回直播赛事信息
     */
     public function index(){
         $json = $this->simpleJson();
-        $list = M("match")->where(array('state'=>array('in',[1,2,3,4])))->field("match_id,league_id,league_name,home_id,home_name,home_score,away_id,away_name,home_score,away_score,home_half_score,away_half_score,home_red,away_red,time,time as match_time,technic,state,total_collect,total_tuijian")->select();
-        $json['sql'] = M()->getLastSql();
+        $list = M("match")->where(array('state'=>array('in',[1,2,3,4])))->order("asc")->field("match_id,league_id,league_name,home_id,home_name,home_score,away_id,away_name,home_score,away_score,home_half_score,away_half_score,home_red,away_red,time,time as match_time,technic,state,total_collect,total_tuijian")->select();
+
         $user_id = intval($this->user['id']);
 
         foreach($list as $i=>$item){
             //
-            $list[$i]['match_time2'] = floor((time()-strtotime($item['time']))/60);
+            $zoudi = M('zoudi')->where(array('match_id'=>$item['match_id']))->order("id DESC")->find();
+            if(!$zoudi){
+                unset($list[$i]);
+                continue;
+            }
+            $zoudi['time'] = str_replace('分','',$zoudi['time']);
+
+            $list[$i]['match_time2'] = is_numeric($zoudi['time'])?$zoudi['time']."'":$zoudi['time'];
+            if($zoudi['time'] == '全场'){
+                $list[$i]['match_time2'] = -1;
+            }
+
             $data = json_decode($item['technic'], true);
             $list[$i]['home_corner'] = isset($data['id6']['home'])?$data['id6']['home']:"0";
             $list[$i]['away_corner'] = isset($data['id6']['away'])?$data['id6']['away']:"0";
--- log.php
+++ log.php
@@ -68,7 +68,7 @@ $data = [
 
 
 //$data['apple_receipt'] = $apple_receipt;
-$result = httpPost("https://api.zydzuqiu.com/live/index.html", $data);
+$result = httpPost("https://api.zydzuqiu.com/match/index.html", $data);
 //var_dump($result);
 echo ($result);
 print_r(json_decode($result, true));
