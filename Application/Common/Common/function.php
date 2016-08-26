9401b3ddc8906b1e3131b56dc153ebce524f4282
ysc8620 <ysc8620@163.com>
2016-08-26 15:12:22 +0800

    ds

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -737,7 +737,7 @@ function get_rate($match_id, $type='',$state=''){
         ];
         $oupei = M('asia_oupei')->where(array('match_id'=>$match_id, 'company_id'=>3))->order('id DESC')->field("begin_home_rate as home_rate, begin_draw_rate as draw_rate, begin_away_rate as away_rate")->find();
         if($oupei){
-            $data = array_merge($data,$oupei);
+            $data = array_merge($data,$oupei);;
         }else{
             $oupei = M('asia_oupei')->where(array('match_id'=>$match_id, 'company_id'=>24))->order('id DESC')->field("begin_home_rate as home_rate, begin_draw_rate as draw_rate, begin_away_rate as away_rate")->find();
             if($oupei){
@@ -755,21 +755,22 @@ function get_rate($match_id, $type='',$state=''){
             }
         }
 
+        // 赛前和上半场去即时盘数据
         if($state == '0' || $state == '1'){
             // 即时盘数据
-            $oupei = M('asia_oupei_change')->where(array('match_id'=>$match_id, 'company_id'=>3))->order('id DESC')->field("change_home_rate, change_draw_rate, change_away_rate")->find();
+            $oupei = M('asia_oupei_change')->where(array('match_id'=>$match_id, 'company_id'=>3))->order('id DESC')->field("change_home_rate as home_win_rate, change_draw_rate as draw_win_rate, change_away_rate as away_win_rate")->find();
             if($oupei){
                 $data = array_merge($data,$oupei);
             }else{
-                $oupei = M('asia_oupei_change')->where(array('match_id'=>$match_id, 'company_id'=>24))->order('id DESC')->field("change_home_rate, change_draw_rate, change_away_rate")->find();
+                $oupei = M('asia_oupei_change')->where(array('match_id'=>$match_id, 'company_id'=>24))->order('id DESC')->field("change_home_rate as home_win_rate, change_draw_rate as draw_win_rate, change_away_rate as away_win_rate")->find();
                 if($oupei){
                     $data = array_merge($data,$oupei);
                 }else {
-                    $oupei = M('asia_oupei_change')->where(array('match_id' => $match_id, 'company_id' => 31))->order('id DESC')->field("change_home_rate, change_draw_rate, change_away_rate")->find();
+                    $oupei = M('asia_oupei_change')->where(array('match_id' => $match_id, 'company_id' => 31))->order('id DESC')->field("change_home_rate as home_win_rate, change_draw_rate as draw_win_rate, change_away_rate as away_win_rate")->find();
                     if ($oupei) {
                         $data = array_merge($data, $oupei);
                     } else {
-                        $oupei = M('asia_oupei_change')->where(array('match_id' => $match_id, 'company_id' => 8))->order('id DESC')->field("change_home_rate, change_draw_rate, change_away_rate")->find();
+                        $oupei = M('asia_oupei_change')->where(array('match_id' => $match_id, 'company_id' => 8))->order('id DESC')->field("change_home_rate as home_win_rate, change_draw_rate as draw_win_rate, change_away_rate as away_win_rate")->find();
                         if ($oupei) {
                             $data = array_merge($data, $oupei);
                         }
@@ -777,9 +778,9 @@ function get_rate($match_id, $type='',$state=''){
                 }
             }
         }
-
+        // 上半场后到结束去走地数据
         if($state == '2' || $state == '3' || $state =='4'){
-            $field = ' `rate_1` as change_home_rate, `rate_2` as change_draw_rate, `rate_3` as change_away_rate';
+            $field = ' `rate_1` as home_win_rate, `rate_2` as draw_win_rate, `rate_3` as away_win_rate';
             // 走地数据
             $oupei = M('zoudi')->where(array('match_id'=>$match_id, 'type'=>array('in', [4,5]), 'company_id'=>3))->order('zoudi_id DESC')->field($field)->find();
             if($oupei){
