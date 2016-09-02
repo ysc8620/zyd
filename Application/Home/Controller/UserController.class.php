743288f01681516a12bd00a1b310c5130cfbcccc
ysc8620 <ysc8620@163.com>
2016-09-02 22:34:22 +0800

    ds

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -31,16 +31,16 @@ class UserController extends BaseApiController {
         unset($member['loss_total']);
         unset($member['zoudi_win_total']);
         unset($member['zoudi_loss_total']);
-        $member['total_rate'] = $member['total_rate'];
+        $member['total_rate'] = number_format($member['total_rate']*100,2,'.','');
         $win = M('tuijian')->where(['is_count'=>1, 'status'=>['in',[1,3]], 'create_time'=>['gt', time()-2592000]])->count();
         $loss = M('tuijian')->where(['is_count'=>1, 'status'=>['in',[2,4]], 'create_time'=>['gt', time()-2592000]])->count();
         $member['win'] = $win;
         $member['loss'] = $loss;
         $member['sql'] = M()->getLastSql();
         $total = $win + $loss;
-        $member['total_month_rate'] = $total > 0?number_format(($win/$total),4,'.','')*100:0.00;
-        $member['before_match_rate'] = $member['before_match_rate']*100;
-        $member['grounder_rate'] = $member['grounder_rate']*100;
+        $member['total_month_rate'] = $total > 0?number_format(($win/$total)*100,4,'.',''):0.00;
+        $member['before_match_rate'] = number_format($member['before_match_rate']*100,2,'.','');
+        $member['grounder_rate'] = number_format($member['grounder_rate']*100,2,'.','');
 
         if(!$mysalf){
             if($member['mobile']){$member['mobile'] = substr_replace($member['mobile'],'*****',3,5);}
