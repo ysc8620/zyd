bc868e7638a53423cd94f602b0688a83a93da8b2
ShengYue <ysc8620@163.com>
2016-10-11 10:43:31 +0800

    ds

--- auto/auto_match_no.php
+++ auto/auto_match_no.php
@@ -23,5 +23,5 @@ and m.time = j.match_time and (m.home_name=j.home_name or m.home_name=j.away_nam
 $res = M()->execute("update `t_match` m, t_jingcai j set j.match_id=m.match_id
 WHERE j.match_id = 0 and j.match_time >=date_format(now(),'%Y-%m-%d 00:00:00')
 and m.time >=date_format(now(),'%Y-%m-%d 00:00:00')
-and (UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) >=300 and UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) <=300 ) and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
+and (UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) >=-300 and UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) <=300 ) and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
 echo date("Y-m-d H:i:s");
