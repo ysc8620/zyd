c7ed549f8e3c63b67787887699a703afafeb2ede
ShengYue <ysc8620@163.com>
2016-10-11 10:23:36 +0800

    ds

--- auto/auto_match_no.php
+++ auto/auto_match_no.php
@@ -15,10 +15,13 @@ require_once __DIR__ .'/config.php';
 echo date("Y-m-d H:i:s")."=auto update match no=\r\n";
 
 $res = M()->execute("update `t_match` m, t_jingcai j set j.match_id=m.match_id
-WHERE j.match_id = 0 and date_format(m.time,'%Y-%m-%d') >=date_format(now(),'%Y-%m-%d') and m.time = j.match_time and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
+WHERE j.match_id = 0 and j.match_time >=date_format(now(),'%Y-%m-%d 00:00:00')
+and m.time >=date_format(now(),'%Y-%m-%d 00:00:00')
+and m.time = j.match_time and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
 
 
 $res = M()->execute("update `t_match` m, t_jingcai j set j.match_id=m.match_id
-WHERE j.match_id = 0 and date_format(m.time,'%Y-%m-%d') >=date_format(now(),'%Y-%m-%d') and date_format(m.time,'%Y-%m-%d') = date_format(j.match_time,'%Y-%m-%d') and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
-
+WHERE j.match_id = 0 and j.match_time >=date_format(now(),'%Y-%m-%d 00:00:00')
+and m.time >=date_format(now(),'%Y-%m-%d 00:00:00')
+and (UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) >=300 and UNIX_TIMESTAMP(m.time) - UNIX_TIMESTAMP(j.match_time) <=300 ) and (m.home_name=j.home_name or m.home_name=j.away_name or m.away_name=j.away_name or m.away_name=j.home_name)");
 echo date("Y-m-d H:i:s");
